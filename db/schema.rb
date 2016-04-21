d2ce436873b629fffb98d7ff82fbbcafecadcbb6
vikas0809 <vikasjoshi93@live.com>
2016-04-21 17:52:45 +0000

    New Book validations completed

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20160420040612) do
   create_table "users", force: :cascade do |t|
     t.string   "firstName",              limit: 255
     t.string   "lastName",               limit: 255
-    t.string   "email",                  limit: 255
     t.string   "contactNumber",          limit: 255
     t.string   "streeAddress",           limit: 255
     t.string   "city",                   limit: 255
@@ -56,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160420040612) do
     t.integer  "userId",                 limit: 4
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
